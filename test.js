02a8f190ada842a75fb479142689c833fc04afad
MatthewLJMU <m.eccleston@2018.ljmu.ac.uk>
2022-03-07 16:19:14 +0000

    ping

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
- res.send('Hello Virtual Machine!')
+ res.send('fuck your life ding dong')
 })
 
 app.get('/Bollocks', (req, res) => {
